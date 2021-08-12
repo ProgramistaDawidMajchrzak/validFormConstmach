6c838abf8713584929f9b14f8eb6dcfb2ac154b9
Dawid Majchrzak <programista.dawid.majchrzak@gmail.com>
2021-08-12 17:15:11 +0200

    Dodanie email.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,42 @@
-
+import emailjs from 'emailjs-com';
 import './App.css';
 import Header from './components/Header.js';
 import FormElement from './components/FormElement.js';
 
 function App() {
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('service_fa2r0jy', 'template_7ctey1i', e.target, 'user_L1OauPxwE4IkKaDjV2sYM')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+    e.target.reset();
+  }
+
   return (
     <div className="container">
       <Header />
-      <form action="">
-        <FormElement labelType={'Nazwa firmy'} inputType={'text'} inputPlaceholder={'Proszę podać nazwę firmy'} />
-        <FormElement labelType={'Imię'} inputType={'text'} inputPlaceholder={'Proszę podać imię'} />
-        <FormElement labelType={'Nazwisko'} inputType={'text'} inputPlaceholder={'Proszę podać nazwisko'} />
-        <FormElement labelType={'Telefon'} inputType={'text'} inputPlaceholder={'Proszę podać numer telefonu'} />
-        <FormElement labelType={'Email'} inputType={'text'} inputPlaceholder={'Proszę podać adres email'} />
-        <FormElement labelType={'Lokalizacja węzła'} inputType={'text'} inputPlaceholder={'Proszę podać lokalizację węzła'} />
-        <FormElement labelType={'Typ węzła'} inputType={'text'} inputPlaceholder={'Proszę podać typ węzła (np. compact, mobicom)'} />
+      <form action="" onSubmit={sendEmail}>
+        <FormElement labelType={'Nazwa firmy'} inputType={'text'} inputPlaceholder={'Proszę podać nazwę firmy'} name={'Firma'} />
+        <FormElement labelType={'Imię'} inputType={'text'} inputPlaceholder={'Proszę podać imię'} name={'Imie'} />
+        <FormElement labelType={'Nazwisko'} inputType={'text'} inputPlaceholder={'Proszę podać nazwisko'} name={'Nazwisko'} />
+        <FormElement labelType={'Telefon'} inputType={'text'} inputPlaceholder={'Proszę podać numer telefonu'} name={'Telefon'} />
+        <FormElement labelType={'Email'} inputType={'text'} inputPlaceholder={'Proszę podać adres email'} name={'Email'} />
+        <FormElement labelType={'Lokalizacja węzła'} inputType={'text'} inputPlaceholder={'Proszę podać lokalizację węzła'} name={'Loaklizacja'} />
+        <FormElement labelType={'Typ węzła'} inputType={'text'} inputPlaceholder={'Proszę podać typ węzła (np. compact, mobicom)'} name={'Typ'} />
         <div className="form-box">
           <label htmlFor="Opis szkody">Opis szkody</label>
-          <textarea name="" id="Opis szkody" cols="30" rows="20" placeholder='Proszę podać informację na temat usterki'></textarea>
+          <textarea name="" id="Opis szkody" cols="30" rows="20" placeholder='Proszę podać informację na temat usterki' name="Opis" ></textarea>
         </div>
         <div className="check-box">
           <input type="checkbox" />
           <p>Wyrażam zgodę na przetwarzanie danych osobowych zgodnie z ustwą o ochronie danych osobowych w związku z (np. wysyłaniem zapytania przez formularz kontaktowy). Podanie danych jest dobrowolne ale niezbędne do przetworzenia zapytania. Zostałem poinformowany, że przysługuje mi prawo dostępu do swoich danych, możliwości ich poprawienia, żądania zaprzestania ich przetwarzania. Administratorem danych osobowych jest Constmach Partner Sp. z o. o.</p>
         </div>
-        <button type="submit">Wyślij</button>
+        <input className='form-submit-btn' type="submit" value='Wyślij' />
       </form>
       <footer><a href="https://constmachpartner.pl/">constmachpartner.pl</a></footer>
     </div>
--- src/components/FormElement.js
+++ src/components/FormElement.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './FormElement.css';
 
 export default function FormElement(props) {
-    const { labelType, inputType, inputPlaceholder } = props;
+    const { labelType, inputType, inputPlaceholder, name } = props;
     return (
         <>
             <div className="form-box">
                 <label htmlFor={labelType}>{labelType}</label>
-                <input id={labelType} type={inputType} placeholder={inputPlaceholder} />
+                <input id={labelType} type={inputType} placeholder={inputPlaceholder} name={name} />
             </div>
         </>
     )
